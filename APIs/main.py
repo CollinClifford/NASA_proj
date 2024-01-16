9569822cef2aaf3fdb83564b5d71e598490aca85
CollinClifford <collinclifford@ymail.com>
2024-01-15 16:54:10 -0800

    Added donki_api trigger to main API file

--- APIs/main.py
+++ APIs/main.py
@@ -9,9 +9,11 @@ logging.basicConfig(filename='logs/main.log', level=logging.DEBUG, format='%(asc
 current_directory = os.path.dirname(os.path.abspath(__file__))
 apod_api = os.path.join(current_directory, 'APOD_API/apod_api.py')
 neows_api = os.path.join(current_directory, 'NeoWs_API/neows_api.py')
+donki_api = os.path.join(current_directory, 'DONKI_API/donki_api.py')
 
 try:
     subprocess.run(['python', apod_api])
     subprocess.run(['python', neows_api])
+    subprocess.run(['python', donki_api])
 except Exception as e:
     logging.error(f'One or more subprocesses failed: {e}')
